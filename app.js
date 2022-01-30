b18dc13f9c3c8ee93e3bf213f9094d16b94ee3f5
BernardKwesi <bernarddormon31@gmail.com>
2022-01-30 18:26:50 +0000

    Adding dotenv package to file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
@@ -12,13 +12,13 @@ var app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
 
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
+
+// view engine setup'mongodb+srv://BernardKwesi:Forever98@cluster0.gmg1u.mongodb.net/local_library?retryWrites=true&w=majority'
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
